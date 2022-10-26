ade78d70848b582978f15bc3d28f0f11d1f82460
mbest <mbest@matthiass-imac.fritz.box>
2022-10-26 17:49:34 +0200

    Initial Versiion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { TemplateHeaderComponent } from './_template/template-header/template-header.component';
+import { TemplateNavigationComponent } from './_template/template-navigation/template-navigation.component';
+import { TemplateRechnerComponent } from './_template/template-rechner/template-rechner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageListComponent,
+    TemplateHeaderComponent,
+    TemplateNavigationComponent,
+    TemplateRechnerComponent
   ],
   imports: [
     BrowserModule
